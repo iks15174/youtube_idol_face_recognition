98b33baa54019e200eafda673d4d882cecd559c0
ParkJiHo <iks15174@daum.net>
2021-11-02 20:22:52 +0900

    draw face box

--- youtubeFaceRecog.py
+++ youtubeFaceRecog.py
@@ -9,6 +9,19 @@ def milToMinute(millis):
     minutes = int(minutes)
     return "{} : {}".format(minutes, seconds)
 
+def drawBox(top, right, bottom, left, frame, name=""):
+    top *= 4
+    right *= 4
+    bottom *= 4
+    left *= 4
+
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    return frame
+
 videoUrl = input('youtube url : ')
 video = pafy.new(videoUrl)
 bestRes = video.getbest(preftype="mp4")
@@ -16,19 +29,19 @@ bestRes = video.getbest(preftype="mp4")
 vidcap = cv2.VideoCapture(bestRes.url)
 print(vidcap)
 
-jinImgae = face_recognition.load_image_file('./img/BTS_JIN.jfif')
+jinImgae = face_recognition.load_image_file('./img/BTS_JIN.jpg')
 jinFaceEncoding = face_recognition.face_encodings(jinImgae)[0]
 
 jinminImgae = face_recognition.load_image_file('./img/BTS_JIMIN.jpg')
 jinminFaceEncoding = face_recognition.face_encodings(jinminImgae)[0]
 
-junggukImgae = face_recognition.load_image_file('./img/BTS_JUNGGUK.jfif')
+junggukImgae = face_recognition.load_image_file('./img/BTS_JUNGGUK.jpg')
 junggukFaceEncoding = face_recognition.face_encodings(junggukImgae)[0]
 
 sugarImgae = face_recognition.load_image_file('./img/BTS_SUGAR.jpg')
 sugarFaceEncoding = face_recognition.face_encodings(sugarImgae)[0]
 
-vImgae = face_recognition.load_image_file('./img/BTS_V.jfif')
+vImgae = face_recognition.load_image_file('./img/BTS_V.jpg')
 vFaceEncoding = face_recognition.face_encodings(vImgae)[0]
 
 knownFaceEncoding = [jinFaceEncoding, jinminFaceEncoding, junggukFaceEncoding, sugarFaceEncoding, vFaceEncoding]
@@ -46,6 +59,9 @@ while True:
     if frameExist:
         time = vidcap.get(cv2.CAP_PROP_POS_MSEC)
 
+    cv2.imshow('frame',frame)
+    key = cv2.waitKey(25)
+
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
@@ -55,12 +71,15 @@ while True:
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_locations = face_recognition.face_locations(rgb_small_frame, model='cnn')
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations, model='large')
+
+        for (top, right, bottom, left) in face_locations:
+            drawBox(top, right, bottom, left, frame)
 
         # print(face_locations)
         face_names = []
-        for face_encoding in face_encodings:
+        for idx, face_encoding in enumerate(face_encodings):
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(knownFaceEncoding, face_encoding)
             name = "Unknown"
@@ -76,9 +95,17 @@ while True:
             if matches[best_match_index]:
                 name = knownFaceName[best_match_index]
                 if name == 'JIN':
+                    print(face_locations[idx])
+                    (top, right, bottom, left) = face_locations[idx]
+                    drawBox(top, right, bottom, left, frame, "진")
                     print(milToMinute(time))
 
             face_names.append(name)
 
+    cv2.imshow('frame',frame)
+    key = cv2.waitKey(25)
+
     process_this_frame = not process_this_frame
+
+
 
