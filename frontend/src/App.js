967b01e2b144010d459ea83d2c8bce7d856a8e5f
ParkJiHo <iks15174@daum.net>
2021-12-22 18:40:35 +0900

    add login form frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
+import React from 'react';
 import { Route, Switch } from 'react-router'
 import { ConnectedRouter } from 'connected-react-router'
+import styled from "styled-components";
+
+import Signup from './container/Signup'
+import Header from './components/common/Header'
+
+const styledDiv = styled.div`
+  width: 100%;
+  height: 100%
+`
 
 function App(props) {
   return (
     <ConnectedRouter history={props.history}>
-      {/* <Header /> */}
-      <div className="App" >
+      <Header />
+      <styledDiv className="App" >
         <Switch>
-          {/* <Route path='/' exact render={() => <React.Fragment><Main /></React.Fragment>} /> */}
+          <Route path='/signup' exact render={() => <React.Fragment><Signup /></React.Fragment>} />
           <Route render={() => <h1>Not Found</h1>} />
         </Switch>
-      </div >
+      </styledDiv >
     </ConnectedRouter>
   );
 }
--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,8 +1,22 @@
-import { connect } from 'react-redux';
 function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <nav class="navbar navbar-expand navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <ul class="d-flex flex-row-reverse navbar-nav mb-2">
+                    <li class="nav-item px-1">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item px-1">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item px-1">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
+
+export default Header
