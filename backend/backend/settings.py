5392aff9f8a23e15ba3664c5a926fab0706ae151
ParkJiHo <iks15174@daum.net>
2021-12-30 23:07:18 +0900

    create faceimage model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import local_settings
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "account",
+    "faceimage",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media files - 업로드를 하는 URL과 디렉토리 설정
+MEDIA_URL = "/media/"  # 업로드 할 경로
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # 로컬 디렉토리 어디에 저장할 것인지
---
+++ backend/faceimage/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+import datetime
+
+
+# Create your models here.
+MyUser = get_user_model()
+
+
+class FaceDetectJob(models.Model):
+    user = models.ForeignKey(
+        MyUser, related_name="face_detect_jobs", on_delete=models.CASCADE
+    )
+    finished = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    finished_at = models.DateTimeField()
+
+    # finished되면 끝난 시간을 함께 저장해준다.
+    def save(self, *args, **kwargs):
+        if self.finished:
+            self.finished_at = datetime.datetime.now()
+
+        super().save(*args, **kwargs)
+
+
+class ImageGroup(models.Model):
+    name = models.CharField(max_length=20)
+    user = models.ForeignKey(
+        MyUser, related_name="image_groups", on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/")
+    user = models.ForeignKey(MyUser, related_name="images", on_delete=models.CASCADE)
+    image_group = models.ForeignKey(
+        ImageGroup, related_name="images", on_delete=models.CASCADE
+    )
